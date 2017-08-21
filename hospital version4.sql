e8227b1f1d98622fcf1b3dbbf896bb91a84e08aa
SumiPrasad <30812898+SumiPrasad@users.noreply.github.com>
2017-08-21 14:17:24 +1200

    Add files via upload

---
+++ hospital version4.sql
@@ -0,0 +1,73 @@
+drop database if exists Hospital;
+create database Hospital;
+use Hospital;
+
+create table ReferFrom(
+typeId int not null auto_increment,
+primary key(typeId),
+refFrom varchar(10)
+)engine=InnoDB;
+
+-- Load external file
+load data infile 'H:\\ARA DATA Wait Lists August 2017.xlsx'
+into table ReferFrom
+-- fields terminated by ','
+lines terminated by '\n'
+(refFrom);
+select * from ReferFrom;
+  
+create table Refree(
+refreeId int not null auto_increment,
+primary key(refreeId),
+refFname varchar(10),
+refLname varchar(10),
+typeId int,
+foreign key(typeId) references ReferFrom(typeId)
+);
+
+create table Patient(
+patientId int not null auto_increment,
+nhi char(7),
+patientFname varchar(10),
+patientLname varchar(10),
+dob date,
+gender char(6),
+hte char(3),
+primary key(patientId)
+);
+
+-- Load external file
+load data infile 'H:\\ARA DATA Wait Lists August 2017.xlsx'
+into table Patient
+fields terminated by ' '
+lines terminated by '\n'
+(nhi,patientFname,patientLname,dob,gender,hte);
+select * from Patient;
+
+create table Department(
+depId int not null auto_increment, 
+primary key(depId),
+depName varchar(10) 
+);
+
+create table Surgeon(
+surgeonId int not null auto_increment,
+sFname varchar(10),
+sLname varchar(10),
+primary key(surgeonId),
+depId int,
+foreign key(depId) references Department(depId)
+);
+
+create table Referal(
+refId int primary key,
+refDate date,
+waitList date,
+fsa date,
+patientId int,
+depId int,
+refreeId int,
+foreign key(depId) references Department(depId),
+foreign key(patientId) references Patient(patientId),
+foreign key(refreeId) references Refree(refreeId)
+);
